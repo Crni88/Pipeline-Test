4bae5917833db1314a83a085e01afa1dce854bed
Tarik Kahrić <tarik.kahric@edu.fit.ba>
2021-12-04 15:21:43 +0100

    Push 7

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Tarik</p>
+      
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
